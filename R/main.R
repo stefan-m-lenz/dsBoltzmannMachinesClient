4048283ab94bcc0ad368b0ee03915786d24d03b4
Stefan Lenz <lenz@imbi.uni-freiburg.de>
2019-07-05 15:06:05 +0200

    Added all fitrbm arguments (not all are working yet)

--- R/main.R
+++ R/main.R
@@ -1,6 +1,39 @@
-ds.monitored_fitrbm <- function(datasources, data, newobj = 'rbm', ...) {
-   # see https://stackoverflow.com/questions/33288387/r-using-the-ellipsis-in-a-call
-   cally <- call('monitored_fitrbmDS', newobj = newobj, data = data)
+ds.monitored_fitrbm <- function(datasources, data, newobj = 'rbm',
+                                monitoringdata = NULL,
+                                # keyword arguments for fitrbm
+                                nhidden = NULL,
+                                epochs = NULL,
+                                upfactor = NULL,
+                                downfactor = NULL,
+                                learningrate = NULL,
+                                learningrates = NULL,
+                                pcd = NULL,
+                                cdsteps = NULL,
+                                batchsize = NULL,
+                                rbmtype = NULL,# TODO
+                                startrbm = NULL)
+{
+   if (!is.null(learningrates)) {
+      learningrates <- paste0(as.character(learningrates), collapse = ",")
+   }
+
+   cally <- call('monitored_fitrbmDS', newobj = newobj, data = data,
+                 monitoringdata = monitoringdata,
+                 # keyword arguments for fitrbm
+                 # (The  order is important (must be same as in server package)
+                 # -  Bug in Opal?)
+                 nhidden = nhidden,
+                 epochs = epochs,
+                 upfactor = upfactor,
+                 downfactor = downfactor,
+                 learningrate = learningrate,
+                 learningrates = learningrates,
+                 pcd = pcd,
+                 cdsteps = cdsteps,
+                 batchsize = batchsize,
+                 rbmtype = rbmtype,
+                 startrbm = startrbm)
+
    monitoringoutput <- datashield.aggregate(datasources, cally)
    return(monitoringoutput)
 }
--- tests/test.R
+++ tests/test.R
@@ -7,5 +7,7 @@ logindata <- data.frame(server = "server",
                         password = "password",
                         table ="50bin.x")
 o <- datashield.login(logins = logindata, assign = TRUE)
-plotEvaluation(ds.monitored_fitrbm(o, data = "D")[[1]])
+ds.monitored_fitrbm(o, data = "D", learningrate = 0.001)
+ds.monitored_fitrbm(o, data = "D", learningrates = rep(0.001, 10), epochs = 10)
+ds.monitored_fitrbm(o, data = "D", learningrates = rep(0.001, 10), epochs = 10, batchsize = 10, pcd = FALSE)
 datashield.logout(o)
